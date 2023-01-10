7066b385fdfeaac9fba768dfee34b2896c2da2fe
yamoo9 <yamoo9@naver.com>
2023-01-10 13:36:50 +0900

    learn. 태그 템플릿 문법
    - 스타일 유틸리티 함수 작성

--- 02-ecmascript-next/src/chapter/02.2-tagged-template-literals.test.js
+++ 02-ecmascript-next/src/chapter/02.2-tagged-template-literals.test.js
@@ -4,23 +4,11 @@ const virtualNode = {
   style: { cssText: '' },
 };
 
-const styled = (node, styles) => {
-  if (typeof styles === 'string') {
-    node.style.cssText = styles;
-  }
-  
-  // node.style.cssText = styles;
-  // CSS 스타일 규칙 객체 → CSS 스타일 문자화
-  if (styles && typeof styles === 'object' && !Array.isArray(styles)) {
-    // 배열의 메서드(reduce) 활용 [객체 → 배열]
-    let updatedStyleString = Object.entries(styles).reduce((cssCode, currentValue) => {
-      let key = currentValue[0];
-      let value = currentValue[1];
-      return `${cssCode}; ${key}: ${value}`;
-    }, '');
-    node.style.cssText = updatedStyleString.trim();
-  }
-  
+const styled = (styles, node) => {
+  // styles 배열
+  let cssRules = styles.reduce((cssRules, currentCssRule) => cssRules + currentCssRule, '');
+  node.style.cssText = cssRules;
+
   return node;
 };
 
@@ -32,17 +20,17 @@ const $styled = () => {
 /* 테스트 코드를 작성합니다. ----------------------------------------------------------- */
 
 describe('스타일 유틸리티 함수', ()=>{
-  // 스타일 코드 작성 (cssMap)
-  const customCssRules = {
-    color: '#4caf51',
-    background: '#1f1910',
-    margin: '0 auto',
-    'max-width': 1240,
-  }
-
   // styled 유틸리티
   test('styled 유틸리티 : styled(virtualNode, cssCode)', () => { 
-    const updatedVNode = styled(virtualNode, customCssRules);
+    const updatedVNode = styled`
+      color: #4caf51;
+      background: #1f1910;
+      margin: 0 auto;
+      max-width: 1280px;
+
+      ${virtualNode}
+    `;
+
     expect(updatedVNode.style.cssText).toMatch(/margin/);
     expect(updatedVNode.style.cssText).toMatch(/max-width/i);
   });
