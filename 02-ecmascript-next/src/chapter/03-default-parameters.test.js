b311abc728ddf0653b3ebfff94fd51161059ca4a
yamoo9 <yamoo9@naver.com>
2023-01-10 14:28:35 +0900

    learn. 기본 매개변수(인수)

--- 02-ecmascript-next/src/chapter/03-default-parameters.test.js
+++ 02-ecmascript-next/src/chapter/03-default-parameters.test.js
@@ -7,18 +7,32 @@ const defaultArg = (value, initialValue) => {
   return value;
 };
 
-const truncateText = (text, limit) => {
-  limit = defaultArg(limit, 100);
+const truncateText = (text, limit = 60) => {
+  // limit = defaultArg(limit, 60);
+  // let a = false || '' || 0 || null || undefined || 100;
+  // limit = limit || 60;
   return `${text.slice(0, limit).trim()}...`;
 };
 
 const desc = `
   기본 매개변수(Default Paramters)는 함수 매개변수의 
   기본 값을 선언적으로 표현하는 강력한 방법입니다.
-`.trim();
+`.trim().replace(/\n\s+/g, '');
 
 /* 테스트 코드를 작성합니다. ----------------------------------------------------------- */
 
 // truncateText(desc) 반환 값의 글자 수
+test('truncateText(desc) 반환 값의 글자 수는 63자이다.', () => {
+  expect(truncateText(desc).length).toBe(63);
+});
 
 // truncateText(desc, 43) 반환 값의 글자 수
+test('truncateText(desc, 43) 반환 값의 글자 수는 43 보다 크고, 46보다 작거나 같다.', () => {
+  let textLength = truncateText(desc, 43).length;
+  expect(textLength).toBeGreaterThan(43);
+  expect(textLength).toBeLessThanOrEqual(46);
+});
+
+test('truncateText(desc, 0) 반환 값의 글자 수는 3자이다.', () => {
+  expect(truncateText(desc, 0).length).toBe(3);
+});
