ad74fcd9606f4f271af0b08447a9d73e7925bda4
yamoo9 <yamoo9@naver.com>
2023-01-10 17:08:39 +0900

    learn. ECMAScript 클래스 구문
    - 클래스 작성
    - 클래스 확장(상속)

--- 02-ecmascript-next/src/chapter/08-class-extends.test.js
+++ 02-ecmascript-next/src/chapter/08-class-extends.test.js
@@ -1,84 +1,48 @@
 /* 아래 ES5 코드를 ES6 코드로 변경합니다. ------------------------------------------------ */
 
-function createClass(classObject, SuperClass) {
-  // @constructor
-  var Class = createClass.extractPropValue(classObject, 'constructor');
-
-  if (!Class) Class = new Function();
-
-  // @static
-  var staticMembers = createClass.extractPropValue(classObject, 'static');
-
-  if (staticMembers) {
-    createClass.exntends(Class, staticMembers);
-  }
-
-  // @prototype
-  createClass.exntends(Class.prototype, classObject);
-
-  // @super
-  var Super = createClass.defaultArg(SuperClass, null);
-
-  if (Super) {
-    createClass.exntends(Class, Super);
-    createClass.exntends(Class.prototype, Super.prototype);
-    Class.prototype.consturctor = Class;
-  }
-
-  return Class;
-}
-
-createClass.defaultArg = function (value, initialValue) {
-  return value === undefined ? initialValue : value;
-};
-
-createClass.extractPropValue = function (object, propName) {
-  var value = object[propName];
-  value && delete object[propName];
-  return value;
-};
-
-createClass.exntends = function (o1, o2) {
-  for (var key in o2) {
-    if ({}.hasOwnProperty.call(o2, key)) {
-      if (!o1[key]) {
-        o1[key] = o2[key];
-      } else {
-        o1[key]._super[key] = o2[key];
-      }
-    }
-  }
-};
+// JavaScript 객체 지향 프로그래밍 
+// - OOJS (with Prototype)
+// - class 문법(syntax) : Java, C#
 
 /* 테스트 코드를 작성합니다. ----------------------------------------------------------- */
 
 // Button 클래스
-var Button = createClass({
-  constructor(type, label) {
-    this.type = type;
-    this.label = label;
-  },
-  static: {
-    version: '1.0.0',
-  },
-  getType: function () {
-    return this.type;
-  },
-});
+class Button {
+  #type;
+  #content;
+
+  constructor(type, content) {
+    this.#type = type;
+    this.#content = content;
+  }
+  
+  static version = '1.0.0';
+
+  get label() {
+    return this.#content;
+  }
+  set label(newLabel) {
+    this.#content = newLabel;
+  }
+  
+  getType() {
+    return this.#type;  
+  }
+  setType(newType) {
+    this.#type = newType;
+  }
+}
 
 // Button 클래스를 확장한 AriaButton 클래스
-var AriaButton = createClass(
-  {
-    constructor(type, label, usingAria) {
-      Button.apply(this, arguments);
-      this.usingAria = createClass.defaultArg(usingAria, true);
-    },
-    static: {
-      displayName: 'AriaButton',
-    },
-    getVersion: function () {
-      return AriaButton.version;
-    },
-  },
-  Button
-);
+const AriaButton = class extends Button {
+  constructor(type, content, usingAria = true) {
+    super(type, content);
+    this.usingAria = usingAria;  
+  }
+
+  static displayName = 'AriaButton';
+
+  getVersion() {
+    return AriaButton.version;
+  }
+}
