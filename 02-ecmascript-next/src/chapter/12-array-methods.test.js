d43a86cbbc87d71a214b10a46cd3acf2ff5302cf
yamoo9 <yamoo9@naver.com>
2023-01-11 10:24:59 +0900

    learn. 배열 메서드 map을 활용한 데이터 수정 (with 비동기 테스트)

--- 02-ecmascript-next/src/chapter/12-array-methods.test.js
+++ 02-ecmascript-next/src/chapter/12-array-methods.test.js
@@ -1,66 +1,46 @@
 /* 배열 메서드(Array Methods)는 데이터를 다룰 때 자주 사용됩니다. ------------------------------- */
+import { fetchBooks, currencyKR } from './utils/index.js';
 
 // -------------------------------------------------------------------------------------
 // 도서 목록 가져오기
 // -------------------------------------------------------------------------------------
 
-function getBooks() {
+// 프로미스
+function getMyBooks() {
+  return fetchBooks()
+    .then((books) => {
+      return books.map((book) => {
+        book.price = currencyKR(book.price);
+        return book;
+      });
+    })
+    .catch((error) => {
+      throw new Error(error.message);
+    });
+}
+
+test('도서 목록 가져오기 (프로미스)', () => {
+  return getMyBooks().then((books) => {
+    expect(typeof books[0].price).toBe('string');
+  });
+});
+
+// 비동기 함수
+async function getBooks() {
   // 모든 도서 목록을 가져온 후, 목록을 순환하여 가격을 "원"화로 변경합니다.
-  // 통신에 실패한 경우 오류 메시지를 throw 합니다.
+  try {
+    const books = await fetchBooks();
+    return books.map((book) => {
+      book.price = currencyKR(book.price);
+      return book;
+    });
+  } catch (error) {
+    // 통신에 실패한 경우 오류 메시지를 throw 합니다.
+    throw new Error(error.message);
+  }
 }
 
-test('도서 목록 가져오기', () => {
-  // 테스트 코드 작성
-  getBooks();
-});
-
-// -------------------------------------------------------------------------------------
-// 제목으로 도서 목록 검색
-// -------------------------------------------------------------------------------------
-
-function searchBooksByTitle(query) {
-  // Async 함수를 활용해보세요.
-  // 모든 도서 목록을 가져온 후, 전달 받은 쿼리와 도서 제목을 포함하는 결과를 반환합니다.
-  // 통신에 실패한 경우 오류 메시지를 throw 합니다.
-}
-
-test('제목으로 도서 목록 검색', () => {
-  // 테스트 코드 작성
-  searchBooksByTitle('파랑');
-});
-
-// -------------------------------------------------------------------------------------
-// 제목 또는 저자로 도서 찾기
-// -------------------------------------------------------------------------------------
-
-function findBook(query, by = 'title') {
-  // Promise 또는 Async 함수 중 1가지 방법을 선택합니다.
-  // 모든 도서 목록을 가져온 후, 전달 받은 매개변수를 사용해 결과(1개)를 반환합니다.
-  // 통신에 실패한 경우 오류 메시지를 throw 합니다.
-}
-
-test('제목으로 도서 찾기', () => {
-  // 테스트 코드 작성
-  findBook('연애');
-});
-
-test('저자로 도서 찾기', () => {
-  // 테스트 코드 작성
-  findBook('정이담', 'author');
-});
-
-// -------------------------------------------------------------------------------------
-// 도서 가격 총합 구하기
-// -------------------------------------------------------------------------------------
-
-function calcurateBooksPrice(query) {
-  // Promise 또는 Async 함수 중 1가지 방법을 선택합니다.
-  // searchBooksByTitle() 함수에 전달 받은 매개변수를 사용해 도서 목록을 가져옵니다.
-  // 도서 목록을 순회해 가격을 모두 더한 결과 값을 반환합니다.
-  // 통신에 실패한 경우 오류 메시지를 throw 합니다.
-}
-
-test('도서 가격 총합 구하기', async () => {
-  // 테스트 코드 작성
-  calcurateBooksPrice('파랑');
+test('도서 목록 가져오기 (비동기 함수)', async () => {
+  const books = await getBooks();
+  expect(typeof books[0].price).toBe('string');
 });
